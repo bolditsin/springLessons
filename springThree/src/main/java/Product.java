d6186c2a654e117fce9eefea375328389d6d2412
bolditsin <70659316+bolditsin@users.noreply.github.com>
2021-05-28 10:56:57 +0300

    SpringThree

---
+++ springThree/src/main/java/Product.java
@@ -0,0 +1,30 @@
+public class Product {
+
+    public int id;
+    public String title;
+    public int amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ springThree/src/main/webapp/WEB-INF/view/product-form-result.jsp
@@ -0,0 +1,14 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+
+<html>
+<body>
+<h1>Product Form Result</h1>
+<br>
+Name: ${product.productName}
+<br>
+Amount: ${product.productAmount}
+
+</body>
+</html>
